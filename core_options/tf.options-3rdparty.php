1bde4ee01b6d43fd3b8aa55ae5503c64c122ab76
Noel Tock <hello@noeltock.com>
2012-03-06 11:52:10 +0100

    Page fix 3rd Party

--- core_options/tf.options-3rdparty.php
+++ core_options/tf.options-3rdparty.php
@@ -15,30 +15,35 @@ function themeforce_3rdparty_overview_page() {
     ?>
     <div class="wrap" id="tf-options-page">
         <div class="tf-text">
-        <h3>What is Social Media, and why you need it?</h3>
-        <p>Social media is a great way for you to stay in touch with your existing customer base, as well as being able to attract others within those private networks. <strong>Social Proof</strong> on the other hand is where your customers will create most of your content (rating, reviews, photos, etc.) and is equally important, so check that out as well!</p>
+        <h3>3rd Party Integrations</h3>
+        <p>By using happytables, we help you <strong>eliminate input duplication</strong> as well as provide integrations to <strong>strong solutions</strong> in other areas of managing a restaurant, i.e. reservations, taking orders, etc.</p>
         <!-- <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/sp-example.jpg'; ?>"> -->
-        <h3 style="margin-top:40px">About the different Social Media Mechanisms</h3>
+        <h3 style="margin-top:40px">About the different Integrations</h3>
         <!-- YELP -->
-        <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/social_facebook.jpg'; ?>" />
-        <p><a href="http://www.facebook.com" target="_blank">Facebook</a> is a social networking service and website launched in February 2004. Users may create a personal profile, add other users as friends, and exchange messages, including automatic notifications when they update their profile. Here are some interesting numbers:</p>
-        <ul>
-            <li>More than <strong>750 million</strong> active users.</li>
-            <li>50% of active users log on to Facebook <strong>everyday</strong>.</li>
-            <li>Average user has <strong>130 friends</strong>.</li>
-        </ul>
-        <div class="social-box tf-settings-wrap"><span><strong>Link</strong> to your Facebook Fan Page </span><a href="<?php echo get_admin_url() . 'admin.php?page=themeforce_facebook'; ?>"><div class="tf-button tf-inline">Go to your Facebook Settings</div></a></div>
+        <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/3rd_opendining.jpg'; ?>" />
+            <p><a href="https://www.opendining.net/link/b1793c2f1bba061d188a11fff40aa30406455fae">Open Dining</a> gets your restaurant more orders, larger tickets, and more loyal customers with web, mobile, and Facebook ordering.</p>
+            <ul>
+                <li>Receive orders in whichever way is most convenient: E-mail, Fax, directly to your PoS and more.</li>
+                <li>The service is available globally.</li>
+            </ul>
+        <div class="social-box tf-settings-wrap"><span><strong>Link</strong> to your Open Dining Menu & Account </span><a href="<?php echo get_admin_url() . 'admin.php?page=themeforce_dining'; ?>"><div class="tf-button tf-inline">Go to your Open Dining Settings</div></a></div>
         <div class="clearfix"></div>
-        <!-- QYPE -->
-        <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/social_twitter.jpg'; ?>" />
-        <p><a href="http://www.twitter.com" target="_blank">Twitter</a> is an online social networking and microblogging service that enables its users to send and read text-based posts of up to 140 characters, informally known as "tweets.".</p>
+
+        <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/3rd_opentable.jpg'; ?>" />
+        <p><a href="http://www.opentable.com/">OpenTable</a> is a leading provider of free, real-time online restaurant reservations for diners and reservation and guest management solutions for restaurants.</p>
         <ul>
-            <li><strong>140 million</strong>. The average number of Tweets people sent per day.</li>
-            <li><strong>460, 000. </strong>Average number of new accounts per day (February 2011).</li>
-            <li><strong>182%</strong>. Increase in number of mobile users over the past year.</li>
+            <li>OpenTable has more than 20,000 restaurant customers, and, since its inception in 1998, has seated more than 200 million diners around the world.</li>
+            <li>The OpenTable service is available throughout the United States, as well as in Canada, Germany, Japan, Mexico, and the United Kingdom.</li>
         </ul>
-        <div class="social-box tf-settings-wrap"><span><strong>Link</strong> to your Twitter Profile </span><a href="<?php echo get_admin_url() . 'admin.php?page=themeforce_twitter'; ?>"><div class="tf-button tf-inline">Go to your Twitter Settings</div></a></div>
+        <div class="social-box tf-settings-wrap"><span><strong>Link</strong> to your Opentable reservations </span><a href="<?php echo get_admin_url() . 'admin.php?page=themeforce_opentable'; ?>"><div class="tf-button tf-inline">Go to your Opentable Settings</div></a></div>
         <div class="clearfix"></div>
+
+        <img src="<?php echo get_bloginfo( 'template_url' ).'/framework/assets/images/3rd_mailchimp.jpg'; ?>" />
+        <p><a href="http://www.mailchimp.com">MailChimp</a> allows you to <strong>automatically</strong> keep in touch with your customer base, updating them on the latest news & events. You can set your newsletter to automatically send off your events on a weekly or monthly basis.</p>
+
+        <div class="social-box tf-settings-wrap"><span><strong>Link</strong> to your MailChimp List </span><a href="<?php echo get_admin_url() . 'admin.php?page=themeforce_mailchimp'; ?>"><div class="tf-button tf-inline">Go to your MailChimp Settings</div></a></div>
+        <div class="clearfix"></div>
+
         </div>
     </div>
     <?php
