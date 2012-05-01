5eeda69e890c64fe604328e37e85a18d59c31782
Noel Tock <hello@noeltock.com>
2012-05-01 21:54:17 +0200

    Schema changes

--- core_seo/tf.schema.php
+++ core_seo/tf.schema.php
@@ -1,34 +1,5 @@
 <?php
 
-/*
---------------------------------------------------------------------------
-Schema.org
---------------------------------------------------------------------------
-Spec used here: http://schema.org/FoodEstablishment
-
-The general concept behind the Schema structure used at Theme Force is a
-modular build whereby the entire body is treated as a schema type (in this
-case a Food Establishment), and various microdata is revealed with visible
-content where possible.
-
-*/
-
-// Open
-
-function tf_open_schema_restaurant() {
-	echo '<div style="margin:0 auto;width:960px;" itemscope itemtype="http://schema.org/Restaurant">';
-}
-
-add_action('tf_body_top', 'tf_open_schema_restaurant', 8);
-
-// Close
-
-function tf_close_schema_restaurant() {
-	echo '</div>';
-}
-
-add_action('tf_body_bottom', 'tf_close_schema_restaurant', 8);
-
 // Special Content (i.e. not easily added inline
 
 function tf_schema_meta() {
