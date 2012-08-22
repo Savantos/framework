166b9865d505b52136c129b131a2daa511ff5295
Noel Tock <hello@noeltock.com>
2012-08-22 14:52:48 +0200

    Image resizing for OG

--- core_seo/tf.open_graph_protocol.php
+++ core_seo/tf.open_graph_protocol.php
@@ -61,33 +61,35 @@ function tf_add_og_meta_tags() {
             $image = $meta_image;
         }
 
-        echo '<meta property="og:image" content="' . $image . '" />';
+        $thumb = wpthumb( $image, 'width=200&height=200&crop=1', false);
+
+        echo '<meta property="og:image" content="' . $thumb . '" />';
 
     endwhile;
 
    // Template specific
 
-        if ( is_single() ) {
+    if ( is_single() ) {
 
-                ?>
+        ?>
 
-                <meta property="og:type" content="article" />
-                <meta property="og:description" content="<?php the_excerpt(); ?>" />
+        <meta property="og:type" content="article" />
+        <meta property="og:description" content="<?php the_excerpt(); ?>" />
 
-                <?php
+        <?php
 
-        }
+    }
 
-        if ( is_front_page() ) {
+    if ( is_front_page() ) {
 
-                ?>
+        ?>
 
-                <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" />
 
-                <?php
+        <?php
 
 
-        } elseif ( !is_front_page() && is_home() ) {
+    } elseif ( !is_front_page() && is_home() ) {
 
         ?>
 
@@ -95,13 +97,13 @@ function tf_add_og_meta_tags() {
 
         <?php
 
-        } elseif ( is_page() ) {
+    } elseif ( is_page() ) {
 
-                ?>
+        ?>
 
-                <meta property="og:type" content="article" />
+        <meta property="og:type" content="article" />
 
-                <?php
+        <?php
 
     }
 
@@ -110,103 +112,5 @@ function tf_add_og_meta_tags() {
     <?php
 
 }
-
-/*
-
-function tf_add_og_meta_tags() {
-	
-	global $post;
-	
-	if ( is_admin() )
-		return;
-	
-	$meta = array();
-	
-    $image = array( 'property' => 'og:image', 'content' => get_option( 'tf_logo' ) );
-	
-	//Site name
-	$meta[] = array( 'property' => 'og:site_name', 'content' => get_bloginfo() );
-	
-	if ( is_single() ) {
-		$meta[] = array( 'property' => 'og:title', 'content' => get_the_title() );
-		$meta[] = array( 'property' => 'og:url', 'content' => get_permalink() );
-		$meta[] = array( 'property' => 'og:description', 'content' => get_the_excerpt() );
-		
-		// Content Type
-		if ( get_post_type() == 'post' ) {
-			
-			$meta[] = array( 'property' => 'og:type', 'content' => 'article' );
-			
-			// Post Image
-			// we dont use get_post_thumbnail_id as we want to be able
-			// to fall back on embadded images etc, which is done through the "post_thumbnail_html"
-			// hook, so we get the html and preg_match the image src
-			
-			$post_thumbnail_html = get_the_post_thumbnail( $post->ID, 'width=100&height=100&crop=1' );
-			
-			if ( $post_thumbnail_html ) {
-				
-				preg_match( '/ src="( [^"]* )/', $post_thumbnail_html, $matches );
-			
-				if ( !empty( $matches[1] ) ) {
-			
-					$post_thumbnail = $matches[1];
-					$meta[] = array( 'property' => 'og:image', 'content' => $post_thumbnail );
-				}
-			} else {
-				$meta[] = $image;
-			}
-		} elseif ( get_post_type() == 'tf_events' ) {
-			
-			$meta[] = array( 'property' => 'og:type', 'content' => 'activity' );
-			
-			// Post Image
-			// we dont use get_post_thumbnail_id as we want to be able
-			// to fall back on embadded images etc, which is done through the "post_thumbnail_html"
-			// hook, so we get the html and preg_match the image src
-			
-			$post_thumbnail_html = get_the_post_thumbnail( $post->ID, 'width=100&height=100&crop=1' );
-			
-			if ( $post_thumbnail_html ) {
-				
-				preg_match( '/ src="( [^"]* )/', $post_thumbnail_html, $matches );
-			
-				if ( !empty( $matches[1] ) ) {
-			
-					$post_thumbnail = $matches[1];
-					$meta[] = array( 'property' => 'og:image', 'content' => $post_thumbnail );
-				}
-			} else {
-				$meta[] = $image;
-			}	
-		
-		} elseif ( get_post_type() == 'tf_foodmenu' ) {
-		
-		}
-		
-	} elseif ( is_front_page() ) {
-		
-		$meta[] = array( 'property' => 'og:type', 'content' => 'restaurant' );
-
-		if ( $phone_number = get_option( 'tf_business_phone' ) ) {
-			$meta[] = array( 'property' => 'og:phone_number', 'content' => $phone_number );
-		}
-		
-		if ( $image ) { $meta[] = $image; }
-		
-	} elseif ( !is_front_page() && is_home() ) {
-		$meta[] = array( 'property' => 'og:type', 'content' => 'blog' );
-		$meta[] = $image;
-	}
-
-	foreach( $meta as $meta_item ) : 
-		if ( $meta_item['content'] ) :?>
-			<meta property="<?php echo $meta_item['property'] ?>" content="<?php echo $meta_item['content'] ?>" />
-		<?php endif;	
-	endforeach;
-
-}
-
-*/
 
 add_action( 'wp_head', 'tf_add_og_meta_tags' );
