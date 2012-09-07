f840dad59e308da82be73264425d47481e8c71b8
Noel Tock <hello@noeltock.com>
2012-09-07 11:28:13 +0300

    Update api_grubhub/tf.grubhub.php
    Updated aff id for gh.

--- api_grubhub/tf.grubhub.php
+++ api_grubhub/tf.grubhub.php
@@ -46,7 +46,7 @@ function tf_grubhub_desktop() {
 
         ?>
 
-        <a style="margin-bottom: 80px;" href="http://www.grubhub.com/order_redir.jsp?custId=<?php echo $id; ?>&amp;affId=#detailsInfo" id="cta-header" class="cta-desktop cta-<?php echo $args["color"]; ?>">
+        <a style="margin-bottom: 80px;" href="http://www.grubhub.com/order_redir.jsp?custId=<?php echo $id; ?>&amp;affId=1101" id="cta-header" class="cta-desktop cta-<?php echo $args["color"]; ?>">
             <span class="cta-icon icon-cart"></span> <span class="cta-headline"><?php echo $args["headline"]; ?></span>
             <div class="poweredby-grubhub-desktop"></div>
         </a>
